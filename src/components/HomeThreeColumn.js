7995671381eb1f76f167d20144fb1e5c2e909fb4
Agata Zyskowska <zysk.ag@gmail.com>
2022-05-12 23:29:07 +0200

    three columns section

--- src/components/HomeThreeColumn.js
+++ src/components/HomeThreeColumn.js
@@ -1,7 +1,34 @@
 import React from "react";
 
 function HomeThreeColumn() {
-  return <section className='three-column'>HomeThreeColumn</section>;
+  return (
+    <section className='three-column'>
+      <div className='three-column-item'>
+        <p className='three-column-item-number'>10</p>
+        <p className='three-column-item-name'>Oddanych worków</p>
+        <p className='three-column-item-text'>
+          Lorem ipsum dolor sit amet, consectetur adipisc Pellentesque vel enim
+          a elit viverra elementuma. Aliquam erat volutpat.
+        </p>
+      </div>
+      <div className='three-column-item'>
+        <p className='three-column-item-number'>5</p>
+        <p className='three-column-item-name'>Wspartych organizacji</p>
+        <p className='three-column-item-text'>
+          Lorem ipsum dolor sit amet, consectetur adipisc Pellentesque vel enim
+          a elit viverra elementuma. Aliquam erat volutpat.
+        </p>
+      </div>
+      <div className='three-column-item'>
+        <p className='three-column-item-number'>7</p>
+        <p className='three-column-item-name'>Zorganizowanych zbiórek</p>
+        <p className='three-column-item-text'>
+          Lorem ipsum dolor sit amet, consectetur adipisc Pellentesque vel enim
+          a elit viverra elementuma. Aliquam erat volutpat.
+        </p>
+      </div>
+    </section>
+  );
 }
 
 export default HomeThreeColumn;
